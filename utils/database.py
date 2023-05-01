4c0c1731cab4fd1a66d0fec9b639780e754f090a
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2023-05-01 23:11:11 +0300

    penis

--- utils/database.py
+++ utils/database.py
@@ -15,7 +15,6 @@ engine = create_async_engine(DATABASE_URL,poolclass=NullPool)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
