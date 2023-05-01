d9795a16cacd98f5e8abfc8e538120d9dd79ab93
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2023-05-01 22:55:05 +0300

    test1

--- utils/database.py
+++ utils/database.py
@@ -14,7 +14,8 @@ DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT
 engine = create_async_engine(DATABASE_URL,poolclass=NullPool)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-
+def test1():
+    pass
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
